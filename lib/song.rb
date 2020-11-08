60b9d97d15eb4276778b8975f0a2a01f93fde669
Kimberly Baker <bakerk2010@gmail.com>
2020-11-08 17:43:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,18 +14,22 @@ class Song
     Song.all.include?(song)
   end
   
-  def Song.new_by_name
-    song = Song.new_by_name("The Middle")
+  def Song.new_by_name(name)
+    song = Song.new_by_name
     song.name
   end
   
-  def Song.create_by_name
-    song = Song.create_by_name("The Middle")
+  def Song.create_by_name(song)
+    song = Song.create_by_name
     song
     Song.all.include?(song)
   end
   
-  def Song.find_by_name
+  def Song.find_by_name(name)
+    the_middle = Song.create_by_name("The Middle")
+    Song.find_by_name("The Middle")
+    
+  end
   
 
 end
